14acbe7d7dfc3f9cb4c0e53321d6f0559db7ba15
lunardidi13 <lunardidi13@gmail.com>
2022-07-01 19:26:36 +0700

    css link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Zayyah</title>
-    <link rel="stylesheet" href="{{ url('css\bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('css\style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css\style.css') }}">
 </head>
 
 {{-- Body --}}
