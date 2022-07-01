a9e4a21ee0d1bb21dfe42e00e66afee4dfac9cce
lunardidi13 <lunardidi13@gmail.com>
2022-07-01 18:34:13 +0700

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container-lg text-center">
+        <h1>Zayyah</h1>
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+            <div class="card border-0">
+                <img src="{{ url('img\skincare-image.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">PRODUK SKIN CARE</h5>
+                    <p class="card-text text-center">JERAWAT, BEKAS JERAWAT, FLEK HITAM, KULIT KUSAM.</p>
+                    <div class="container text-center">
+                        <a class="btn btn-dark" href="#" role="button">View Product</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border-0">
+                <img src="{{ url('img\skincare-image.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">PRODUK SKIN CARE</h5>
+                    <p class="card-text text-center">JERAWAT, BEKAS JERAWAT, FLEK HITAM, KULIT KUSAM.</p>
+                    <div class="container text-center">
+                        <a class="btn btn-dark" href="#" role="button">View Product</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border-0">
+                <img src="{{ url('img\skincare-image.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">PRODUK SKIN CARE</h5>
+                    <p class="card-text text-center">JERAWAT, BEKAS JERAWAT, FLEK HITAM, KULIT KUSAM.</p>
+                    <div class="container text-center">
+                        <a class="btn btn-dark" href="#" role="button">View Product</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border-0">
+                <img src="{{ url('img\skincare-image.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">PRODUK SKIN CARE</h5>
+                    <p class="card-text text-center">JERAWAT, BEKAS JERAWAT, FLEK HITAM, KULIT KUSAM.</p>
+                    <div class="container text-center">
+                        <a class="btn btn-dark" href="#" role="button">View Product</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
